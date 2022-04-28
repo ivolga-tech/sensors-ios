0230cb3c32d48bede6e63f161557675dbca5f821
pbaskov <pbaskov8@gmail.com>
2022-04-28 10:58:34 +0300

    Framework.

---
+++ MobileSensors.xcframework/ios-arm64_x86_64-simulator/MobileSensors.framework/Headers/MobileSensors.h
@@ -0,0 +1,16 @@
+//
+//  MobileSensors.h
+//  MobileSensors
+//
+//  Created by Pavel Baskov on 16.02.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MobileSensors.
+FOUNDATION_EXPORT double MobileSensorsVersionNumber;
+
+//! Project version string for MobileSensors.
+FOUNDATION_EXPORT const unsigned char MobileSensorsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MobileSensors/PublicHeader.h>
