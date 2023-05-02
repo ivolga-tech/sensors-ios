5070ffd0611845c238dfafc31a4d77a889c0b71c
pbaskov <pbaskov8@gmail.com>
2023-05-02 20:20:35 +0300

    Framework update.

---
+++ HumanSignals.xcframework/watchos-arm64_i386_x86_64-simulator/HumanSignals.framework/Headers/HumanSignals.h
@@ -0,0 +1,16 @@
+//
+//  HumanSignals.h
+//  HumanSignals
+//
+//  Created by Pavel Baskov on 16.02.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for HumanSignals.
+FOUNDATION_EXPORT double HumanSignalsVersionNumber;
+
+//! Project version string for HumanSignals.
+FOUNDATION_EXPORT const unsigned char HumanSignalsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HumanSignals/PublicHeader.h>
